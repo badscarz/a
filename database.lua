2047c8025f5cfd7c0ce478d46a32c289a8631db8
badscarz <badscarz@gmail.com>
2026-03-06 01:16:21 -0500

    Create database.lua

---
+++ database.lua
@@ -0,0 +1,14 @@
+return {
+    Main = {
+        {name = "Combat FPS Hub", url = "https://tsulnavhfrykjkbqiuaw.supabase.co/functions/v1/raw-script?id=e69521da-3b9b-4e8e-b275-d33b290cdcac"},
+        {name = "Krypton Game Files", url = "https://tsulnavhfrykjkbqiuaw.supabase.co/functions/v1/raw-script?id=fe3b5b71-2192-4215-8d89-9f5cc5566abc"},
+        {name = "OTHER SCRIPTS", url = "OPEN_SUB_GUI"}, -- This triggers the scroll list
+        {name = "TERMINATE ALL", url = "ACTION_TERMINATE"}
+    },
+    Sub = {
+        {name = "Bubble Gum Simulator Infinity", load = "loadstring(game:HttpGet('URL_HERE'))()"},
+        {name = "Blade Ball", load = "loadstring(game:HttpGet('URL_HERE'))()"},
+        {name = "PLACEHOLDER 1", load = "print('P1')"},
+        {name = "PLACEHOLDER 2", load = "print('P2')"}
+    }
+}
